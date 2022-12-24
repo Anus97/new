e4157c1b456bd9192159fa0170d3200aa6be0a98
Anu S <anusaratha97@gmail.com>
2022-12-25 00:15:47 +0530

    first commit

---
+++ login_page/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+import Navbar from './Components/Navbar';
+function App() {
+  return (
+    <div className="App">
+        <Components/>
+    </div>
+  );
+}
+
+export default App;
